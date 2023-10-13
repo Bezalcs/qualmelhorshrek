7039ea7cf9522f0d9441b09c48d7f71f340fec65
Ana Beatriz <beatrizabreushh@gmail.com>
2023-10-12 21:30:42 -0300

    terceiro commit

---
+++ main.py
@@ -0,0 +1,7 @@
+pergunta = input("qual é o melhor filme do shreK?")
+if pergunta == "shrek 1" or pergunta == "shrek 2" :
+    print("certa resposta!")
+elif  pergunta == "shrek 3" or  pergunta == "shrek 4" :
+    print("meu deus vc é louco?")
+else:
+    print("invalida")
