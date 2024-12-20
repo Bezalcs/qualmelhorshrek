ba4c035342cbbf6b2bbbe7d5f24f1d133ab388ec
Bezalcs <beza.lucas@hotmail.com>
2024-12-20 10:14:24 -0300

    melhorando as condições

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-pergunta = input("qual é o melhor filme do shreK?")
-if pergunta == "shrek 1" or pergunta == "shrek 2" :
-    print("certa resposta!")
-elif  pergunta == "shrek 3" or  pergunta == "shrek 4" :
-    print("meu deus vc é louco?")
-else:
-    print("invalida")
+pergunta = input("qual é o melhor filme do shreK? ")
+resposta_certa = str('Shrek 2')
+pior_resposta = str('Shrek 4')
+
+pergunta = pergunta.capitalize()
+
+if pergunta == resposta_certa:
+    print('Resposta certa!!!')
+elif pergunta == pior_resposta:
+    print('Você esta louco?')
+elif    pergunta == 'Shrek 1' or pergunta == 'Shrek 3':
+    print('resposta errada')
+else: print('Voce digitou o nome do filme errado')
